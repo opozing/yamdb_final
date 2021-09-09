c046c9e911755c5132b17b4ad5cfcbb50d53b4db
opozing <gnom213@yandex.ru>
2021-09-09 23:14:41 +0300

    1

--- users/models.py
+++ users/models.py
@@ -14,6 +14,17 @@ class YamdbUserManager(BaseUserManager):
     Обеспечивает правильную реализацию команды 'manage.py createsuperuser'
     """
 
+    def create_superuser(self, username=None, password=None, email=None):
+        user_obj = self.create_user(
+            username=str(uuid1()),
+            email=email,
+            password=password,
+            is_staff=True,
+            is_admin=True,
+            is_active=True,
+        )
+        return user_obj
+
     def create_user(self, email,
                     username,
                     password=None,
@@ -33,17 +44,6 @@ class YamdbUserManager(BaseUserManager):
         user_obj.is_superuser = is_admin
         user_obj.is_active = is_active
         user_obj.save(using=self._db)
-        return user_obj
-
-    def create_superuser(self, username=None, password=None, email=None):
-        user_obj = self.create_user(
-            username=str(uuid1()),
-            email=email,
-            password=password,
-            is_staff=True,
-            is_admin=True,
-            is_active=True,
-        )
         return user_obj
 
 
